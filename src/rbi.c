a5fa009c4e641c721eb599cf032f4d86d75827fe
YiPing Pan <pan.yiping.fi@gmail.com>
2023-02-20 21:46:22 +0800

    Add CMake files and src/ directory

--- src/rbi.c
+++ src/rbi.c
@@ -482,33 +482,3 @@ static node_t *lookup(node_t *node, int value) {
   }
   return node;
 }
-
-int main(int argc, char **argv) {
-  (void)argc;
-  tree_new(&tree);
-  { node_t *nodei = nodes;
-    for (++argv; *argv; ++argv, ++nodei) {
-      int v, n;
-      if ((char*)nodei == (char*)nodes + sizeof(nodes)) {
-        fprintf(stderr, "fatal: out of node memory\n");
-        return 2;
-      }
-      if (sscanf(*argv, "%d%n", &v, &n) <= 0 || strlen(*argv) != n + 0U) {
-        fprintf(stderr, "fatal: bad number in arg: %s\n", *argv);
-        return 1;
-      }
-      printf("insert: %d\n", v);
-      nodei->value = v;
-      tree_insert(&tree, nodei);
-    }
-  }
-  { node_t * const node = lookup(tree.rbt_root, 7);
-    printf("---\nlookup(7): %d\n---\n", node ? node->value : -1);
-  }
-  {
-    const int sum = sum_subtree(tree.rbt_root);
-    printf("---\nsum: %d\n", sum);
-  }
-
-  return 0;
-}
